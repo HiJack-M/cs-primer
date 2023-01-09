3a27d2260ac4d44232748aba8e5bda34a1c596b7
HiJack <hijack.sherlock@live.com>
2023-01-09 16:38:48 +0800

    feat(js basic): promise

--- js-basic/promise.js
+++ js-basic/promise.js
@@ -11,3 +11,70 @@ promise.then(
     console.log(reason)
   }
 )
+
+console.log('hahaha')
+
+// new Promise((resovle, reject) => {
+//   setTimeout(() => resovle(1), 1000)
+// })
+//   .then((result) => {
+//     console.log(result)
+//     return new Promise((resovle, reject) => {
+//       setTimeout(() => resovle(result * 2), 1000)
+//     })
+//   })
+//   .then((result) => {
+//     console.log(result)
+//     return new Promise((resovle, reject) => {
+//       setTimeout(() => resovle(result * 2), 1000)
+//     })
+//   })
+//   .then((result) => {
+//     console.log(result)
+//   })
+
+class Thenable {
+  constructor(value) {
+    this.value = value
+  }
+
+  then(resovle, reject) {
+    console.log('resolve: ', resovle)
+    console.log('value: ', this.value)
+    resovle(this.value)
+  }
+}
+
+new Promise((resovle, reject) => {
+  resovle(1)
+})
+  .then((result) => {
+    return new Thenable(result)
+  })
+  .then(console.log)
+
+new Promise((resolve, reject) => {
+  resolve('ok')
+})
+  .then((result) => {
+    nosuchfunction()
+  })
+  .catch((error) => {
+    console.log(error.message)
+  })
+
+Promise.all([
+  new Promise((resolve) => setTimeout(() => resolve(1), 3000)),
+  new Promise((resolve) => setTimeout(() => resolve(2), 2000)),
+  new Promise((resolve) => setTimeout(() => resolve(3), 1000)),
+]).then(console.log)
+
+async function loadJson(url) {
+  let res = await fetch(url)
+  if (res.status == 200) {
+    return await res.json()
+  }
+  throw new Error(res.status)
+}
+
+loadJson('https://javascript.info/no-such-user.json').catch(alert)
