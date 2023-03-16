68fee55e62dcc54aac9af8a5786432e8ad04a9b9
HiJack <hijack.sherlock@live.com>
2023-03-16 11:30:58 +0800

    feat(js-basic): decorator

--- js-basic/decorator.js
+++ js-basic/decorator.js
@@ -126,3 +126,83 @@ function delay(func, time) {
     setTimeout(() => func.apply(this, args), time)
   }
 }
+
+// Debounce decorator
+
+// The result of debounce(f, ms) decorator is a wrapper that suspends calls to f until there’s ms milliseconds of inactivity (no calls, “cooldown period”), then invokes f once with the latest arguments.
+
+// In other words, debounce is like a secretary that accepts “phone calls”, and waits until there’s ms milliseconds of being quiet. And only then it transfers the latest call information to “the boss” (calls the actual f).
+
+// For instance, we had a function f and replaced it with f = debounce(f, 1000).
+
+// Then if the wrapped function is called at 0ms, 200ms and 500ms, and then there are no calls, then the actual f will be only called once, at 1500ms. That is: after the cooldown period of 1000ms from the last call.
+
+// …And it will get the arguments of the very last call, other calls are ignored.
+
+// Here’s the code for it (uses the debounce decorator from the Lodash library):
+
+let f_debounce = debounce(function (x) {
+  console.log(x)
+}, 1000)
+
+f_debounce('a')
+setTimeout(() => f_debounce('b'), 200)
+setTimeout(() => f_debounce('c'), 500)
+// debounced function waits 1000ms after the last call and then runs: alert("c")
+
+function debounce(func, ms) {
+  let timer
+
+  return function () {
+    clearTimeout(timer)
+    timer = setTimeout(() => func.apply(this, arguments), ms)
+  }
+}
+
+// Throttle decorator
+
+// Create a “throttling” decorator throttle(f, ms) – that returns a wrapper.
+
+// throttle runs it not more often than given ms time. Good for regular updates that shouldn’t be very often.
+
+// In other words, throttle is like a secretary that accepts phone calls, but bothers the boss (calls the actual f) not more often than once per ms milliseconds.
+
+function f_throttle(a) {
+  console.log(a)
+}
+
+// f1000 passes calls to f at maximum once per 1000 ms
+let ff1000 = throttle(f_throttle, 1000)
+
+ff1000(1) // shows 1
+ff1000(2) // (throttling, 1000ms not out yet)
+ff1000(3) // (throttling, 1000ms not out yet)
+
+// when 1000 ms time out...
+// ...outputs 3, intermediate value 2 was ignored
+
+function throttle(func, ms) {
+  let isThrottled = false
+  let savedThis
+  let savedArgs
+
+  return function () {
+    if (isThrottled) {
+      savedThis = this
+      savedArgs = arguments
+      return
+    }
+    isThrottled = true
+
+    func.apply(this, arguments)
+
+    setTimeout(() => {
+      if (savedArgs) {
+        isThrottled = false
+        func.apply(savedThis, savedArgs)
+        savedThis = null
+        savedArgs = null
+      }
+    }, ms)
+  }
+}
